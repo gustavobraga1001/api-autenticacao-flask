4f2b62b36bee2b6cca35d9c179922492f6272049
gustavobraga1001 <braga.gustavo@outlook.com.br>
2025-01-10 13:35:06 -0300

    implementando funcao base de autenticacao

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+from models.user import User # linha que adicionei
+from database import db 
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
+login_manager = LoginManager()
+db.init_app(app)
+login_manager.init_app(app)
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    print(username, password)
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return jsonify({"message": "Autenticação realizada com sucesso"})        
+                
+    return jsonify({"message": "Credenciais inválidas"}), 400
+
+
 
 @app.route("/", methods=["GET"])
 def hello_world():
